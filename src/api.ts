5a71ed9e9716ff803b5f4c836241c0d6fa507266
Sven Kroflin <sven.kroflin@gmail.com>
2023-05-14 12:29:52 +0200

    dodane crud funkcije u apiu i popravljen query client provider

--- src/api.ts
+++ src/api.ts
@@ -9,8 +9,51 @@ function apiGetCall(route: string) {
         }).then((res) => res.data)
 }
 
+function apiUpateCall<T>(rout: string, data: T) {
+    return axios.post(`/api${route}`, {
+        headers: {
+            'Access-Control-Allow-Credentials': 'true',
+            'Access-Control-Allow-Origin': "*"
+        },
+        data: data
+    }).then((res) => res.data)
+}
+
+function apiDeleteCall<T>(route: string, data: T) {
+    return axios.delete(`/api/${route}`, {
+            headers: {
+                'Access-Control-Allow-Credentials': 'true',
+                'Access-Control-Allow-Origin': "*"
+            },
+            data: data
+        }).then((res) => res.data)
+}
+
+function apiUpdateCall<T>(route: string, data: T) {
+    return axios.put(`/api/${route}`, {
+        headers: {
+            'Access-Control-Allow-Credentials': 'true',
+            'Access-Control-Allow-Origin': "*"
+        },
+        data: data
+    }).then((res) => res.data)
+}
+
+export interface EmployeeDeleteReq {
+    employeeName: string
+}
+
+export interface EmployeeUpdateReq {
+    salary: number,
+    departmentName: string,
+    departmentLocation: string,
+    employeeName: string
+}
+
 // employees
 export const getEmployees = () => apiGetCall("employee")
-export const getDepartments = () => apiGetCall("department")
+export const deleteEmployee = (req: EmployeeDeleteReq) => apiDeleteCall<EmployeeDeleteReq>("employee", req)
+export const updateEmployee = (req: EmployeeUpdateReq) => apiUpdateCall<EmployeeUpdateReq>("employee", req)
 
 // department
+export const getDepartments = () => apiGetCall("department")
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { QueryClientProvider, QueryClient } from 'react-query';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import 'semantic-ui-css/semantic.min.css';
 import { SideBar } from './Components/SideBar.tsx';
 import { Employee } from './Components/Employee/Employee.tsx';
-import { Department } from './Components/Department.tsx';
+import { Department } from './Components/Department/Department.tsx';
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-      <SideBar links={[
-        { id: "Employee", icon: "user", url: "/", component: <Employee /> },
-        { id: "Department", icon: "building", url: "department", component: <Department /> },
-      ]} />
+      <QueryClientProvider client={queryClient}>
+        <SideBar links={[
+          { id: "Employee", icon: "user", url: "/", component: <Employee /> },
+          { id: "Department", icon: "building", url: "department", component: <Department /> },
+        ]} />
+      </QueryClientProvider>
 )
