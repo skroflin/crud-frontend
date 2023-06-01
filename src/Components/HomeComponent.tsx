341d81a34764cca8712ce164319161dd2b3fb1e4
Sven Kroflin <sven.kroflin@gmail.com>
2023-06-01 14:51:50 +0200

    povecan icon i dodan style

--- src/Components/HomeComponent.tsx
+++ src/Components/HomeComponent.tsx
@@ -2,16 +2,18 @@ import React from "react";
 import { Header, Icon } from "semantic-ui-react";
 import styles from './Styles.module.scss'
 
-export function HomeComponent(){
+export function HomeComponent() {
     return (
         <div className={styles.home}>
             <div className={styles.content}>
-            <Header as='h1' icon textAlign='center' className={styles.header}>
-                <Icon name='users' circular className={styles.icon}/>
-                <Header.Content className={styles.subcontent}>Welcome User</Header.Content>
-                <Header.Subheader className={styles.Subheader}>This is my application.</Header.Subheader>
-                <Header.Subheader>Consists of basic CRUD functions.</Header.Subheader>
-            </Header>
+                <Header as='h1' icon textAlign='center' className={styles.header}>
+                    <Icon.Group size="big">
+                        <Icon name='users' circular/>
+                    </Icon.Group>
+                    <Header.Content className={styles.subcontent} as='h1'>Welcome User</Header.Content>
+                    <Header.Subheader className={styles.subheader}>This is my application.</Header.Subheader>
+                    <Header.Subheader className={styles.subheader}>Consists of basic CRUD functions.</Header.Subheader>
+                </Header>
             </div>
         </div>
     )
