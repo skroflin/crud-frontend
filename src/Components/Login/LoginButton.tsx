bd1154438d315972da42bf1df745c13f1b80f67c
Sven Kroflin <sven.kroflin@gmail.com>
2023-06-03 06:42:54 +0200

    icon

--- src/Components/Login/LoginButton.tsx
+++ src/Components/Login/LoginButton.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
-import { Button } from 'semantic-ui-react';
+import { Button, Icon } from 'semantic-ui-react';
 import styles from "../Styles.module.scss";
 
 export function LoginButton() {
     return (
         <div className={styles.main_div}>
             <Button.Group className={styles.main_button} floated='right'>
-                <Button className={styles.login_button}>
-                    Login
+                <Button className={styles.login_button} animated='vertical'>
+                    <Button.Content hidden>
+                        Login
+                    </Button.Content>
+                    <Button.Content visible>
+                        <Icon color='black' name='sign in' size='large' />
+                    </Button.Content>
                 </Button>
             </Button.Group>
         </div>
